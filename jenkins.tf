7b71fadde602552adfcfc89df9ef46836cbbe114
Corentin Beuf <corentin.beuf@ynov.com>
2022-04-16 12:11:23 +0200

    Create file jenkins.tf

---
+++ jenkins.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "Telmate/proxmox"
+      version = "2.9.6"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url = "https://192.168.1.2:8006/api2/json/access/ticket"
+  pm_user="terraform-prov@pve"
+  pm_password="terraform"
+}
+
+resource "proxmox_vm_qemu" "resource-name" {
+    name        = "NAME"
+    target_node = "factory"
+    clone = "template-debian"
+    memory = 2048
+    oncreate = true
+    onboot = true
+    network {
+        bridge    = "vmbr2"
+        tag = 10
+        firewall  = false
+        link_down = false
+        model     = "virtio"
+    }
+    disk {
+    type = "virtio"
+    storage = "DATA"
+    size = "32G"
+  }
+}
